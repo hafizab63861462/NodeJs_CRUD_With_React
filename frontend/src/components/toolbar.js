6ad53df7cb50f9701acaa63ebca7d5c8c972ce98
hafiz_abdullah <hafiz.abdullahoffical@gmail.com>
2024-04-27 00:10:28 +0500

    remove paragraph

--- frontend/src/components/toolbar.js
+++ frontend/src/components/toolbar.js
@@ -12,7 +12,6 @@ function ToolbarAction() {
 
     }}>
       <Button variant="contained" onClick={() => { setOpen(true) }}>Add User</Button>
-      <p style={{ marginLeft: '10px' }}>If want to update anu user click on row</p>
       <SideDialog open={open} setOpen={setOpen} />
     </div >
   )
