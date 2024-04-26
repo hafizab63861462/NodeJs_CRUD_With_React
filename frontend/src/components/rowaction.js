543ff1ce4896c1fe982966401cf6a80301fdcf67
hafiz_abdullah <hafiz.abdullahoffical@gmail.com>
2024-04-27 00:05:06 +0500

    soft delete

--- frontend/src/components/rowaction.js
+++ frontend/src/components/rowaction.js
@@ -3,16 +3,44 @@ import { IconButton, Box } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import SideDialog from './dialog'
+import { UPDATEUSER } from '../graphhql/Mutation/useMutationUpdateUser';
+
+import { useMutation } from "@apollo/client";
 
 function RowAction({ selectedRow }) {
   const [open, setOpen] = useState(false);
+
+  const [updateUser] = useMutation(UPDATEUSER, {
+    awaitRefetchQueries: true,
+    refetchQueries: ["getUsers"],
+    onCompleted: (data) => {
+      if (data) {
+        if (data?.updateUser?.success) {
+          alert("User update successful")
+        } else {
+          alert(`issue ${data?.updateUser?.message}`);
+        }
+      }
+    },
+  });
+
+  const handleDelete = () => {
+    updateUser({
+      variables: {
+        user: {
+          ...selectedRow,
+          isDeleted: true
+        }
+      }
+    });
+  }
 
   return (
     <Box>
       <IconButton onClick={() => setOpen(true)}>
         <EditIcon />
       </IconButton>
-      <IconButton onClick={() => console.info('Delete')}>
+      <IconButton onClick={handleDelete}>
         <DeleteIcon />
       </IconButton>
       <SideDialog open={open} setOpen={setOpen} selectedRow={selectedRow} />
